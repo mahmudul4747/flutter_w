031fc298db59e88a65e4ecc19dcaf6418f28e769
Imran_mahmud <mahmudulimran396@gmail.com>
2026-05-16 15:30:47 +0600

    Update

--- lib/mayproject/all_Screen/Student_list.dart
+++ lib/mayproject/all_Screen/Student_list.dart
@@ -1,5 +1,6 @@
+import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:http/http.dart' as http;
 
 class StudentList extends StatefulWidget {
   const StudentList({super.key});
@@ -10,355 +11,361 @@ class StudentList extends StatefulWidget {
 
 class _StudentListState extends State<StudentList> {
 
-  List<Map<String, String>> students = [];
+  List<Map<String, dynamic>> students = [];
 
   final TextEditingController nameController = TextEditingController();
   final TextEditingController emailController = TextEditingController();
   final TextEditingController departmentController = TextEditingController();
   final TextEditingController semesterController = TextEditingController();
-  final _formKey = GlobalKey<FormState>();
+ 
+ List<Map<String, dynamic>> filteredStudents = [];
 
-  // Add Student Function
-  void addStudent(){
-    if(nameController.text.isNotEmpty
-    && emailController.text.isNotEmpty
-    && departmentController.text.isNotEmpty
-    && semesterController.text.isNotEmpty
-    ){
+final TextEditingController searchController = TextEditingController();
+  @override
+  void initState() {
+    super.initState();
+   Future.delayed(Duration.zero, () async {
+  await getStudents();
+});
+  }
+  void searchStudent(String value) {
+
+  setState(() {
+
+    filteredStudents = students.where((student) {
+
+      final name =
+          student['name'].toString().toLowerCase();
+
+      return name.contains(value.toLowerCase());
+
+    }).toList();
+
+  });
+
+}
+
+
+  // GET 
+  
+  Future<void> getStudents() async {
+
+  try {
+
+    var url = Uri.parse("http://10.0.2.2/student_api/read.php");
+
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+
+      final data = jsonDecode(response.body);
+
       setState(() {
-        students.add({
-          'name': nameController.text,
-          'email': emailController.text,
-          'department': departmentController.text,
-          'semester': semesterController.text,
-        });
+
+        students = List<Map<String, dynamic>>.from(data);
+
+        filteredStudents = students;
+
       });
-      // Clear Fields
-      nameController.clear();
-      emailController.clear();
-      departmentController.clear();
-      semesterController.clear();
-      Navigator.pop(context);
+
     }
-  } 
-   // Dialog Box
-  void showAddStudentDialog(){
+
+  } catch (e) {
+
+    print("Error: $e");
+
+  }
+
+}
+  //  ADD 
+  Future<void> addStudentAPI() async {
+
+    var url = Uri.parse("http://10.0.2.2/php/stu_create.php");
+
+    await http.post(url, body: {
+
+      "name": nameController.text,
+      "email": emailController.text,
+      "department": departmentController.text,
+      "semester": semesterController.text,
+
+    });
+
+    await getStudents();
+  }
+
+  // DELETE 
+  Future<void> deleteStudentAPI(String id) async {
+
+    var url = Uri.parse("http://10.0.2.2/php/stu_delet.php");
+
+    await http.post(url, body: {
+      "id": id,
+    });
+
+    await getStudents();
+  }
+
+  // Update
+  Future<void> updateStudentAPI(String id) async {
+
+    var url = Uri.parse("http://10.0.2.2/php/stu_update.php");
+
+    await http.post(url, body: {
+
+      "id": id,
+      "name": nameController.text,
+      "email": emailController.text,
+      "department": departmentController.text,
+      "semester": semesterController.text,
+
+    });
+
+    await getStudents();
+  }
+
+  // ADD DIALOG
+  void showAddStudentDialog() {
 
     showDialog(
       context: context,
-      builder: (context){
+      builder: (context) {
 
         return AlertDialog(
 
           title: const Text("Add Student"),
 
-          content: Form(
-            key: _formKey,
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: [
-                TextFormField(
-                  controller: nameController,
-                  decoration: const InputDecoration(
-                    hintText: "Enter student name",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                TextFormField(
-                  controller: emailController,
-                  decoration: const InputDecoration(
-                    hintText: "Enter student email",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                TextFormField(
-                  controller: departmentController,
-                  decoration: const InputDecoration(
-                    hintText: "Enter student department",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-                TextFormField(
-                  controller: semesterController,
-                  decoration: const InputDecoration(
-                    hintText: "Enter student semester",
-                    border: OutlineInputBorder(),
-                  ),
-                ),
-              ],
-            ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+
+              TextField(controller: nameController),
+              TextField(controller: emailController),
+             // TextField(controller: departmentController),
+             // TextField(controller: semesterController),
+
+            ],
           ),
-          
 
           actions: [
 
             TextButton(
-              onPressed: (){
-                Navigator.pop(context);
-              },
+              onPressed: () => Navigator.pop(context),
               child: const Text("Cancel"),
             ),
 
             ElevatedButton(
-              onPressed: addStudent,
+              onPressed: () async {
+
+                await addStudentAPI();
+
+                nameController.clear();
+                emailController.clear();
+                departmentController.clear();
+                semesterController.clear();
+
+                Navigator.pop(context);
+
+              },
               child: const Text("Add"),
             ),
 
           ],
         );
-
       },
     );
-
   }
-  void deleteStudent(int index){
 
-  setState(() {
-
-    students.removeAt(index);
-
-  });
-
-}
-void editStudent(int index){
-
-  nameController.text = students[index]['name']!;
-  emailController.text = students[index]['email']!;
-  departmentController.text = students[index]['department']!;
-  semesterController.text = students[index]['semester']!;
-
-  showDialog(
-
-    context: context,
-
-    builder: (context){
-
-      return AlertDialog(
-
-        title: const Text("Update Student"),
-
-        content: SingleChildScrollView(
-
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-
-            children: [
-
-              TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  labelText: "Name",
-                ),
-              ),
-
-              const SizedBox(height: 10),
-
-              TextField(
-                controller: emailController,
-                decoration: const InputDecoration(
-                  labelText: "Email",
-                ),
-              ),
-
-              const SizedBox(height: 10),
-
-              TextField(
-                controller: departmentController,
-                decoration: const InputDecoration(
-                  labelText: "Department",
-                ),
-              ),
-
-              const SizedBox(height: 10),
-
-              TextField(
-                controller: semesterController,
-                decoration: const InputDecoration(
-                  labelText: "Semester",
-                ),
-              ),
-
-            ],
-
-          ),
-
-        ),
-
-        actions: [
-
-          TextButton(
-
-            onPressed: (){
-              Navigator.pop(context);
-            },
-
-            child: const Text("Cancel"),
-
-          ),
-
-          ElevatedButton(
-
-            onPressed: (){
-
-              setState(() {
-
-                students[index] = {
-
-                  'name': nameController.text,
-                  'email': emailController.text,
-                  'department': departmentController.text,
-                  'semester': semesterController.text,
-
-                };
-
-              });
-
-              Navigator.pop(context);
-
-            },
-
-            child: const Text("Update"),
-
-          ),
-
-        ],
-
-      );
-
-    },
-
-  );
-
-}
-
-
+  //  UI 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
-   floatingActionButton: FloatingActionButton(
+
+      floatingActionButton: FloatingActionButton(
         onPressed: showAddStudentDialog,
+        backgroundColor: const Color.fromARGB(214, 240, 219, 37),
         child: const Icon(Icons.add),
       ),
 
-      
       body: Container(
-        color: const Color.fromARGB(255, 245, 221, 8),
+        color: const Color.fromARGB(214, 240, 219, 37),
+
         child: Column(
+
           children: [
-            Container(
-              height: 150,
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: const Color.fromARGB(255, 245, 221, 8),
-                borderRadius: BorderRadius.circular(35),
-              ),
-              child: Center(
-                child: Text(
-                  "Student List",
-                  style: TextStyle(
-                    fontSize: 36,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.black87,
-                  ),
-                ),
-              ),
+
+            const SizedBox(height: 40),
+
+            const Text(
+              "Student List",
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
             ),
 
-            SizedBox(height: 20),
+            const SizedBox(height: 20),
 
             Expanded(
+
               child: Container(
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.circular(30),
-                ),
-                child: ListView.builder(
 
-                  itemCount: students.length,
+  decoration: const BoxDecoration(
+    color: Colors.white,
+    borderRadius: BorderRadius.vertical(
+      top: Radius.circular(30),
+    ),
+  ),
 
-                  itemBuilder: (context,index){
+  child: Column(
 
-                    return ListTile(
-                    
+    children: [
 
-                      leading: CircleAvatar(
+      // SEARCH BAR
+      Padding(
 
-                            radius: 30,
+  padding: const EdgeInsets.all(15),
 
-                            backgroundImage: AssetImage(
-                              "assets/plant15.png",
-                            ),
+  child: Container(
 
-                          ),
+    decoration: BoxDecoration(
 
-                      title: Text(
-                        students[index]['name']!,
-                        style: const TextStyle(
-                          fontSize: 20,
-                        ),
-                      ),
-                      subtitle: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
+      borderRadius: BorderRadius.circular(25),
 
-                            Text("Email: ${students[index]['email']}"),
+      boxShadow: [
 
-                            Text("Department: ${students[index]['department']}"),
+        BoxShadow(
 
-                            Text("Semester: ${students[index]['semester']}"),
+          color: const Color.fromARGB(214, 240, 219, 37),
 
-                          ],
-                        ),trailing: PopupMenuButton(
+          spreadRadius: 3,
 
-                          onSelected: (value){
+          blurRadius: 10,
 
-                            if(value == 'edit'){
-
-                              editStudent(index);
-
-                            }
-
-                            else if(value == 'delete'){
-
-                              deleteStudent(index);
-
-                            }
-
-                          },
-
-                          itemBuilder: (context) => [
-
-                            const PopupMenuItem(
-                              value: 'edit',
-                              child: Text("Edit"),
-                            ),
-
-                            const PopupMenuItem(
-                              value: 'delete',
-                              child: Text("Delete"),
-                            ),
-
-                          ],
-
-                        ), 
-
-                    );
-
-                
-                  
-                  }
-                ),
-              )
-            
-
-            ),
-            
-          ],
+          offset: const Offset(0, 3),
 
         ),
+
+      ],
+
+    ),
+
+    child: TextField(
+
+      controller: searchController,
+
+      onChanged: searchStudent,
+
+      decoration: InputDecoration(
+
+        hintText: "Search Student",
+
+        prefixIcon: const Icon(Icons.search),
+
+        filled: true,
+
+        fillColor: Colors.white,
+
+        border: OutlineInputBorder(
+
+          borderRadius: BorderRadius.circular(25),
+
+          borderSide: BorderSide.none,
+
+        ),
+
       ),
-    
+
+    ),
+
+  ),
+
+),
+      // LIST
+      Expanded(
+
+        child: ListView.builder(
+
+          itemCount: filteredStudents.length,
+
+          itemBuilder: (context, index) {
+
+            return ListTile(
+
+              leading: const CircleAvatar(),
+
+              title: Text(
+                filteredStudents[index]['name'] ?? "",
+              ),
+
+              subtitle: Column(
+                crossAxisAlignment:
+                    CrossAxisAlignment.start,
+
+                children: [
+
+                  Text(
+                    "Email: ${filteredStudents[index]['email']}",
+                  ),
+
+                  Text(
+                    "Dept: ${filteredStudents[index]['department']}",
+                  ),
+
+                  Text(
+                    "Sem: ${filteredStudents[index]['semester']}",
+                  ),
+
+                ],
+              ),
+
+              trailing: PopupMenuButton(
+
+                onSelected: (value) async {
+
+                  if (value == 'delete') {
+
+                    await deleteStudentAPI(
+                      filteredStudents[index]['id']
+                          .toString(),
+                    );
+
+                  }
+
+                },
+
+                itemBuilder: (context) => const [
+
+                  PopupMenuItem(
+                    value: 'edit',
+                    child: Text("Edit"),
+                  ),
+
+                  PopupMenuItem(
+                    value: 'delete',
+                    child: Text("Delete"),
+                  ),
+
+                ],
+
+              ),
+
+            );
+
+          },
+
+        ),
+
+      ),
+
+    ],
+
+  ),
+
+),
+
+            )
+          ],
+        ),
+      ),
     );
   }
 }
