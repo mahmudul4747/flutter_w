46716c30944608efa9cc82b49c9808d32264296f
Imran_mahmud <mahmudulimran396@gmail.com>
2026-06-07 23:49:19 +0600

    Update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,21 +1,43 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
 android {
-    compileSdk = 34
+    namespace = "com.example.flutter_w"
+
+    compileSdk = 36
 
     defaultConfig {
-        targetSdk = 34
+        applicationId = "com.example.flutter_w"
+        minSdk = flutter.minSdkVersion
+        targetSdk = 36
+        versionCode = 1
+        versionName = "1.0"
     }
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
-        coreLibraryDesugaringEnabled = true
+        isCoreLibraryDesugaringEnabled = true
     }
 
     kotlinOptions {
         jvmTarget = "17"
     }
+
+    buildTypes {
+        release {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
 }
 
 dependencies {
-    coreLibraryDesugaring "com.android.tools:desugar_jdk_libs:2.0.4"
+    coreLibraryDesugaring("com.android.tools:desugar_jdk_libs:2.0.4")
+}
+
+flutter {
+    source = "../.."
 }
